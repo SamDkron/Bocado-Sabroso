5ed9b2a439a352bd1b0bdca68818ca0345de31e9
Samuel Dau <91066714+SamDkron@users.noreply.github.com>
2025-05-18 20:59:53 -0500

    funciones agregadas al archivo restaurante

--- restaurante.py
+++ restaurante.py
@@ -43,4 +43,42 @@ class Restaurante:
         
         mesa["ocupado"] = True  # Marcar como ocupada
         return mesa["id"]   # Retornar el id de la mesa
+
+        def liberar_mesa(self, id_mesa: str): #Se le ingresa el id de la mesa que se quiere liberar
+        for i in range(self.mesas.filas):
+            for j in range(self.mesas.columnas):
+                if self.mesas.matriz[i][j]["id"] == id_mesa: #Busca la mesa en la matriz
+                    self.mesas.matriz[i][j]["ocupado"] = False #Marca la mesa como no ocupada
+                    return
+        raise ValueError("La mesa no existe o no está ocupada") #Si no se encuentra la mesa o si está vacía, lanza una excepción
     
+    def zona_mesas_mas_utilizadas(self):
+        uso_mesas = {2: {"total": 0, "ocupadas": 0, "libres": 0},
+                    4: {"total": 0, "ocupadas": 0, "libres": 0},  #crea un diccionario para almacenar el uso de las mesas
+                    8: {"total": 0, "ocupadas": 0, "libres": 0}}
+
+        for fila in self.mesas.matriz:
+            for mesa in fila: #recorre la matriz de mesas
+                capacidad = mesa["capacidad"] #obtiene la capacidad de la mesa
+                uso_mesas[capacidad]["total"] += 1 #le suma 1 al numero total de mesas de esa capacidad
+                if mesa["ocupado"]:
+                    uso_mesas[capacidad]["ocupadas"] += 1 #le suma 1 al numero de mesas ocupadas de esa capacidad
+                else:
+                    uso_mesas[capacidad]["libres"] += 1 #le suma 1 al numero de mesas libres de esa capacidad
+        return uso_mesas
+    
+
+    ##comentarios antes de la función:
+    ##1. las capacidades de las mesas son 2, 4 y 8 y se ejecuta una vez el for para cada una de ellas
+    ##2. el diccionario de uso de mesas es un diccionario que almacena la cantidad total de mesas de cada capacidad, la cantidad de mesas ocupadas y la cantidad de mesas libres
+
+    
+    def calcular_tasa_ocupacion(self, uso_mesas): #llama al diccionario de uso de mesas
+        for capacidad, valores in uso_mesas.items(): #recorre el diccionario de uso de mesas
+            total = valores["total"] #obtiene el total de mesas de la capacidad que estamos trabajando
+            ocupadas = valores["ocupadas"] #obtiene el total de mesas ocupadas de esa capacidad
+            if total > 0: #si el numero total de mesas de la capacidad que estamos trabajando es mayor a 0
+                tasa_ocupacion = (ocupadas / total) * 100 #calcula la tasa de ocupación
+            else:
+                tasa_ocupacion = 0 #si no hay mesas de esa capacidad, la tasa de ocupación es 0
+        print(f"Capacidad {capacidad}: Tasa de ocupación: {tasa_ocupacion:.2f}%")
